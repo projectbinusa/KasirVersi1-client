f34218776e96aac4d728a18ce4b666bc3749b46e
Irvanda Ibrahim <irvandaibrahim.tkj.2021@binusasmg.sch.id>
2023-01-16 15:05:44 +0700

    update

--- src/components/IsiBils.jsx
+++ src/components/IsiBils.jsx
@@ -1,12 +1,60 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { API_AUTH } from '../utils/baseURL';
 
 function IsiBils() {
-    return (
-        <div>
 
+    
+    return (
         <div className='m-5'>
-            hola
-        </div>
+            <h1 className="font-bold text-4xl">History Checkout</h1>
+            <div className=' p-5 bg-gray-50 col-span-9 h-screen overflow-y-auto scroll-none"'>
+                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <table class="w-full text-sm text-left text-gray-500 ">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-200 ">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">
+                                    Product name
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Color
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Category
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Price
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Action
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="bg-white border-b ">
+                               
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                                    Apple MacBook Pro 17"
+                                </th>
+                                <td class="px-6 py-4">
+                                    Sliver
+                                </td>
+                                <td class="px-6 py-4">
+                                    Laptop
+                                </td>
+                                <td class="px-6 py-4">
+                                    $2999
+                                </td>
+                                <td class="px-6 py-4">
+                                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                </td>
+                            </tr>
+                           
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
 
         </div>
     )
