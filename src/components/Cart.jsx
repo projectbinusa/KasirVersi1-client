72edb511499f6f73833e8770444afc6f1c3941f1
RizqiRamandika <rizqiramandika96@gmail.com>
2023-01-18 11:50:10 +0700

    update

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -224,7 +224,7 @@ function Cart({ dataCart, setDataCart }) {
 
   useEffect(() => {
     getToko();
-  }, [])
+  }, []);
   return (
     <div id="cart">
       <div id="nav-logo">
@@ -502,8 +502,10 @@ function Cart({ dataCart, setDataCart }) {
                     Back
                   </button>
                   <button
-                    // onClick={checkout}
-                    onClick={handleGeneratePdf}
+                    onClick={() => {
+                      handleGeneratePdf;
+                      checkout;
+                    }}
                     data-modal-hide="defaultModal"
                     type="submit"
                     className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
